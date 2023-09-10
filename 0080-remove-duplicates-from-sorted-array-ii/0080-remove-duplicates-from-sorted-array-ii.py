299583ae1b84a97e744a916b1b8d9a83d17f7447
정영희 <69373688+jkhyjkhy@users.noreply.github.com>
2023-09-10 19:07:12 +0900

    Time: 61 ms (51.74%), Space: 16.4 MB (51.04%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,12 +1,9 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-            if len(nums) <= 2:
-                return len(nums)
-
-            index = 2  # 중복을 최대 두 번까지만 허용하는 경우
-            for i in range(2, len(nums)):
-                if nums[i] != nums[index - 2]:
-                    nums[index] = nums[i]
-                    index += 1
-
-            return index
+        i = 0
+        for n in nums:
+            if i < 2 or n != nums[i-2]:
+                nums[i] = n
+                i += 1
+        return i
+                
