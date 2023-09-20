10d3d73c9c7663b24c66c9d7e97e2ea26ab30632
정영희 <69373688+jkhyjkhy@users.noreply.github.com>
2023-09-20 18:48:47 +0900

    Time: 45 ms (88.56%), Space: 16.2 MB (81.12%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,29 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        romans = {
+            1 : 'I',
+            4 : 'IV',
+            5 : 'V',
+            9 : 'IX',
+            10 : 'X',
+            40 : 'XL',
+            50 : 'L',
+            90 : 'XC',
+            100 : 'C',
+            400 : 'CD',
+            500 : 'D',
+            900 : 'CM',
+            1000 : 'M'
+        }
+        
+        result = ""
+        
+        for number, roman in sorted(romans.items(), reverse = True): 
+            count = num // number
+            num %= number
+            result += roman * count
+        
+        return result
+        
+        
+        
