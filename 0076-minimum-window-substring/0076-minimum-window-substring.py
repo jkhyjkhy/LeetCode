e6299ec5861d6636ddf16dcdc1800b987bd83a4b
정영희 <69373688+jkhyjkhy@users.noreply.github.com>
2023-10-12 11:36:00 +0900

    Time: 149 ms (35.77%), Space: 16.9 MB (96.91%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,18 +1,29 @@
-# 다시 풀어볼 문제
-# 내 코드가 동작하지 않아 
-
-# StefanPochmann의 12 lines Python 해설을 참고함
 from collections import Counter
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        need, missing = Counter(t), len(t)
-        i = I = J = 0
-        for j, c in enumerate(s, 1):
-            missing -= need[c] > 0
-            need[c] -= 1
-            if not missing:
-                while need[s[i]] < 0: need[s[i]] += 1; i += 1
-                if not J or j - i <= J - I: I, J = i, j
-                need[s[i]] += 1; i += 1; missing += 1       # SPEEEEEEEED UP!
-        return s[I : J]
+        if not s or not t:
+            return ""
+        
+        count = len(t)
+        counter = Counter(t)
+        left = 0
+        min_len = float('inf')
+        min_window = ""
+        
+        for right in range(len(s)):
+            if counter[s[right]] > 0:
+                count -= 1
+            counter[s[right]] -= 1
+            
+            while count == 0:
+                if right - left + 1 < min_len:
+                    min_len = right - left + 1
+                    min_window = s[left:right + 1]
+                
+                if counter[s[left]] == 0:
+                    count += 1
+                counter[s[left]] += 1
+                left += 1
+        
+        return min_window
