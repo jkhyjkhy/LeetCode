8ede2ed37a8f7cc8f4bfcd1a3115e32a4dfcc723
정영희 <69373688+jkhyjkhy@users.noreply.github.com>
2023-10-12 11:52:05 +0900

    Time: 109 ms (68.05%), Space: 17 MB (79.51%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,28 +2,24 @@ from collections import Counter
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not s or not t:
-            return ""
-        
         count = len(t)
         counter = Counter(t)
+        window = ""
         left = 0
-        min_len = float('inf')
-        min_window = ""
-        
+        min_window=max_window = 0
+
         for right in range(len(s)):
-            if counter[s[right]] > 0:
-                count -= 1
+            count -= counter[s[right]] > 0
             counter[s[right]] -= 1
-            
-            while count == 0:
-                if right - left + 1 < min_len:
-                    min_len = right - left + 1
-                    min_window = s[left:right + 1]
-                
-                if counter[s[left]] == 0:
+            if count == 0:
+                while left < right and counter[s[left]] < 0:
+                    counter[s[left]] += 1
+                    left += 1
+                if not max_window or right - left <= max_window - min_window:
+                    max_window, min_window = right, left
+                    window = s[min_window:max_window+1]
                     count += 1
-                counter[s[left]] += 1
-                left += 1
-        
-        return min_window
+                    counter[s[left]] += 1
+                    left += 1
+                    
+        return window
