06960dcce7fc7d8e405d6b77d92c549bfadb73de
정영희 <69373688+jkhyjkhy@users.noreply.github.com>
2023-11-10 19:09:11 +0900

    Time: 43 ms (18.71%), Space: 16.2 MB (74.72%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -3,10 +3,12 @@ class Solution:
         ranges = []
         current_range = []
         start_index = 0
-        if len(nums) == 1:
+        
+        if len(nums) == 1: # 배열의 요소가 하나면
             ranges.append(rangesToString(nums[0],nums[0]))
             return ranges
-        for i in range(1, len(nums)):
+        
+        for i in range(1, len(nums)): # 아니라면
             if nums[i] == nums[i - 1] + 1:
                 if i == len(nums)-1: # 순회가 끝나면
                     ranges.append(rangesToString(nums[start_index], nums[i]))
@@ -18,7 +20,7 @@ class Solution:
                     ranges.append(rangesToString(nums[start_index], nums[i]))
         return(ranges)
 
-def rangesToString(a: int, b: int) -> str:
+def rangesToString(a: int, b: int) -> str: # 정답 양식에 맞는 전환 함수
     if a == b:
         return(f"{a}")
     else:
